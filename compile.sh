50c52b5f220575f0c5127be623dd53a8c381f7fa
Dario Bartussek <d.bartussek@gmail.com>
2021-03-27 14:26:43 +0100

    Updated main to actually produce a start function

--- compile.sh
+++ compile.sh
@@ -1,3 +1,5 @@
 cargo fmt
 
-RUSTFLAGS="-C link-args=-T3dsx.ld -C link-args=-Map=3ds.map" cargo xbuild --target 3ds.json
+RUSTFLAGS="-C link-args=-T3dsx.ld -C link-args=-Map=rust_3ds.map" cargo xbuild --target 3ds.json
+
+llvm-objdump -d -C target/3ds/debug/rust_3ds > rust_3ds.s
