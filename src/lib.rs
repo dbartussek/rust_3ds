ab04f21be18f48b44bd43a19db352541370f27a5
Dario Bartussek <d.bartussek@gmail.com>
2021-04-21 13:23:01 +0200

    Updated libctru bindings

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
 #![feature(restricted_std)]
 
-use core::ptr::null_mut;
 use libctru::raw::{
-    aptMainLoop, c_char, c_int, consoleInit, gfxExit, gfxFlushBuffers, gfxInitDefault,
-    gfxScreen_t_GFX_BOTTOM, gfxScreen_t_GFX_TOP, gfxSwapBuffers, gspWaitForEvent, hidKeysDown,
-    hidScanInput, GSPGPU_Event_GSPGPU_EVENT_VBlank0, KEY_START,
+    aptMainLoop, consoleInit, gfxExit, gfxFlushBuffers, gfxInitDefault, gfxScreen_t_GFX_BOTTOM,
+    gfxSwapBuffers, gspWaitForEvent, hidKeysDown, hidScanInput, GSPGPU_Event_GSPGPU_EVENT_VBlank0,
+    KEY_START,
+};
+use std::{
+    os::raw::{c_char, c_int},
+    ptr::null_mut,
 };
 
 #[no_mangle]
@@ -31,19 +34,6 @@ pub extern "C" fn main(_argc: c_int, _argv: *const *const c_char) -> c_int {
 
     unsafe {
         gfxInitDefault();
-
-        consoleInit(gfxScreen_t_GFX_TOP, null_mut());
-
-        libctru::println!("Standard print in next line:");
-        println!("Standard print");
-
-        std::thread::spawn(|| {
-            println!("Hello thread world");
-        })
-        .join()
-        .unwrap();
-
-        libctru::println!("Check end");
 
         while aptMainLoop() {
             hidScanInput();
